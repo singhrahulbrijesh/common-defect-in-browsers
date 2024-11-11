e30aaba85fee411fb6e4f225e0a63e38a40317ef
Rahul Singh <82301632+singhrahulbrijesh@users.noreply.github.com>
2024-11-11 13:12:18 -0500

    Update bugfix-commits-subject-fx.sql

--- sql/bugfix-commits-subject-fx.sql
+++ sql/bugfix-commits-subject-fx.sql
@@ -1,1 +1,2 @@
+---This command is to copy particular columns to database from CSV file
 \copy (select a.commit, b.bug_id, a.subject, b.churn from fx_git_commit a, fx_bugfix_commits b where a.commit = b.commit and b.churn > 0 order by b.churn desc) to '~/Documents/CommonDefects/bugfix-commits-subject-fx.csv' DELIMITER ',' CSV HEADER;
