198788cc51c88867d52a6beeed422b5e0273aa51
Rahul Singh <82301632+singhrahulbrijesh@users.noreply.github.com>
2024-11-11 13:11:56 -0500

    Update bugfix-commits-subject-ch.sql

--- sql/bugfix-commits-subject-ch.sql
+++ sql/bugfix-commits-subject-ch.sql
@@ -1,1 +1,2 @@
+---This command is to copy particular columns to database from CSV file
 \copy (select a.commit, b.bug_id, a.subject, b.churn from ch_git_commit a, ch_bugfix_commits b where a.commit = b.commit and b.churn > 0 order by b.churn desc) to '~/Documents/CommonDefects/bugfix-commits-subject-ch.csv' DELIMITER ',' CSV HEADER;
